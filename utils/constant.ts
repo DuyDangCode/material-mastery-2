62f892ca28822b822c41834c2a3c78065a4d134f
Thành Duy <21520774@gm.uit.edu.vn>
2025-01-10 15:29:53 +0700

    update url

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,8 +1,8 @@
-const BASE_URL = 'http://13.215.48.173'
+const BASE_URL = 'http://13.215.48.173/v1/api'
 const API_KEY =
   '8f07a25f2b0b6d30f9895090c579178ca93b5d53ad83d1deb4f12dc41678babdf60572e005b0d5de933b17ea167968c2f7398ebb1b484e5a4f500adaf42f7ea7'
 const USER_COOKIE_NAME = 'user'
-const SOCKET_URL = 'http://54.153.228.47/'
+const SOCKET_URL = 'http://13.215.48.173/'
 
 export const constant = {
   BASE_URL,
