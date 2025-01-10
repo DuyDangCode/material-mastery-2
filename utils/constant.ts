b8fdd1221f906d91cd9e2fb2bd81671b2473114d
Thành Duy <21520774@gm.uit.edu.vn>
2025-01-10 15:28:29 +0700

    update url

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,11 +1,12 @@
-const BASE_URL = 'https://mm.khangnt.space/v1/api'
-const API_KEY = '8f07a25f2b0b6d30f9895090c579178ca93b5d53ad83d1deb4f12dc41678babdf60572e005b0d5de933b17ea167968c2f7398ebb1b484e5a4f500adaf42f7ea7'
+const BASE_URL = 'http://13.215.48.173'
+const API_KEY =
+  '8f07a25f2b0b6d30f9895090c579178ca93b5d53ad83d1deb4f12dc41678babdf60572e005b0d5de933b17ea167968c2f7398ebb1b484e5a4f500adaf42f7ea7'
 const USER_COOKIE_NAME = 'user'
-const SOCKET_URL = 'http://mm.khangdev.id.vn/';
+const SOCKET_URL = 'http://54.153.228.47/'
 
 export const constant = {
-    BASE_URL,
-    API_KEY,
-    USER_COOKIE_NAME,
-    SOCKET_URL,
+  BASE_URL,
+  API_KEY,
+  USER_COOKIE_NAME,
+  SOCKET_URL,
 }
